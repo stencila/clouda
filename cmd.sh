082feaad49328307528901da0edf9f64b9834e0d
Nokome Bentley <me@noko.me>
2017-08-15 08:46:15 +1200

    Start kubectrl proxy

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Run kubectl proxy for local access to the API
+# See https://kubernetes.io/docs/tasks/access-application-cluster/access-cluster/#accessing-the-api-from-a-pod
+kubectl proxy --port=8000 &
+
 # Authenticate with gcloud
 if test -e "/mnt/secrets/gcloud-key-file.json"
 then
@@ -7,5 +11,5 @@ then
 	gcloud auth activate-service-account --key-file /mnt/secrets/gcloud-key-file.json
 fi
 
-# Serve server.js
+# Serve host
 npm run serve
