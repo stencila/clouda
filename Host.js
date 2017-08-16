6e7b221fb54817f9fb00be516fdacf492d7001a6
Nokome Bentley <me@noko.me>
2017-08-16 19:11:57 +1200

    Using signed cookies for session management

--- Host.js
+++ Host.js
@@ -1,79 +1,217 @@
-const sibyl = require('stencila-sibyl')
-const alru = require('array-lru')
-const jwt = require('jsonwebtoken')
-const stream = require('stream')
-const uuid = require('uuid')
+const crypto = require('crypto')
+const Docker = require('dockerode')
+const kubernetes = require('kubernetes-client')
+const pino = require('pino')()
+const randomPort = require('random-port')
+const request = require('retry-request')
 
-// Create an LRU object that can hold up to 30 streams. Destroyed once evicted
-// to prevent memory leaking
-const lru = alru(30, {
-  evict: function (index, stream) {
-    if (stream.destroy) stream.destroy()
-    else stream.end()
-  }
+const HostHttpServer = require('./HostHttpServer')
+
+// Configuration settings
+const NODE_ENV = process.env.NODE_ENV
+
+const STENCILA_IMAGE = process.env.STENCILA_IMAGE || 'stencila/alpha'
+
+var TOKEN_KEY = process.env.TOKEN_KEY
+if (!TOKEN_KEY) {
+  if (NODE_ENV === 'development') TOKEN_KEY = 'a super unsecet key'
+  else throw Error('TOKEN_KEY must be set')
+}
+
+// During development, Docker is used to create session containers
+const docker = new Docker({
+  socketPath: '/var/run/docker.sock'
 })
 
-module.exports = Sibyl
+// In production, Kubernetes is used to create session containers
+const k8s = new kubernetes.Core({
+  url: 'http://127.0.0.1:8000'
+})
 
-function Sibyl (log) {
-  if (!(this instanceof Sibyl)) return new Sibyl(log)
-  this.log = log
-}
+/**
+ * Implements the Stencila Host API using a Kubernetes cluster
+ */
+class Host {
+  /**
+   * Sign a session
+   *
+   * Generates a HMAC-SHA256 signature of the session that is used for verification.
+   *
+   * @param  {object} session The session object
+   * @return {string}         The session signature (a hex digest)
+   */
+  sign (session) {
+    return crypto.createHmac('sha256', TOKEN_KEY).update(JSON.stringify(session)).digest('hex')
+  }
 
-Sibyl.prototype.get = function (id) {
-  this.log.debug('connecting to stream', id)
-  return lru.get(id)
-}
-
-// TODO: make sure the tokens are validated
-Sibyl.prototype.open = function (address, opts) {
-  opts = opts || {}
-
-  var closed = false
-  const self = this
-
-  const source = new stream.PassThrough()
-  const sink = new stream.PassThrough()
-
-  sibyl('open', address, source, onExit)
-  source.on('data', parseMessage)
-
-  const id = uuid()
-  lru.set(id, sink)
-
-  return id
-
-  function parseMessage (data) {
-    if (closed) return
-    for (let line of data.toString().split('\n')) {
-      if (line.length) {
-        const match = line.match(/^(STEP|IMAGE|GOTO) (.+)$/)
-        if (match) {
-          let type = match[1]
-          let data = match[2]
-          if (type === 'STEP') {
-            self.log.debug('SSE: sending step', data)
-            sink.write(`event: step\ndata: ${data}\n\n`)
-          } else if (type === 'IMAGE') {
-            self.log.debug('SSE: sending image', data)
-            sink.write(`event: image\ndata: ${data}\n\n`)
-          } else if (type === 'GOTO') {
-            const token = jwt.sign({ url: data }, process.env.TOKEN_SECRET, { expiresIn: '12h' })
-            self.log.debug('SSE: sending stdout goto', token)
-            sink.write(`event: goto\ndata: ${token}\n\n`)
-          }
-        } else {
-          self.log.debug('SSE: sending stdout data', line)
-          sink.write(`event: stdout\ndata: ${line}\n\n`)
-        }
+  checkin (token) {
+    let session
+    if (token) {
+      const json = Buffer.from(token, 'base64').toString()
+      const object = JSON.parse(json)
+      if (object.signature !== this.sign(object.session)) return null
+      session = object.session
+    } else {
+      session = {
+        start: new Date()
       }
+    }
+    pino.info({ session: session }, 'checkin')
+    return session
+  }
+
+  // Checkout a session by creating a token for it
+  checkout (session) {
+    pino.info({ session: session }, 'checkout')
+    const object = {
+      session: session,
+      signature: this.sign(session)
+    }
+    const json = JSON.stringify(object)
+    const token = Buffer.from(json).toString('base64')
+    return token
+  }
+
+  spawn (cb) {
+    if (NODE_ENV === 'development') {
+      // During development use Docker to run a new container
+      randomPort((port) => {
+        const options = {
+          Image: STENCILA_IMAGE,
+          ExposedPorts: { '2000/tcp': {} },
+          HostConfig: {
+            PortBindings: {
+              '2000/tcp': [{
+                'HostIp': '127.0.0.1',
+                'HostPort': port.toString()
+              }]
+            }
+          }
+        }
+        docker.createContainer(options, (err, container) => {
+          if (err) return cb(err)
+          container.start((err) => {
+            if (err) return cb(err)
+            cb(null, `http://127.0.0.1:${port}`)
+          })
+        })
+      })
+    } else {
+      // In production, use Kubernetes to create a new pod
+      const name = 'pod-' + crypto.randomBytes(24).toString('hex')
+      const port = 2000
+      k8s.ns.pods.post({ body: {
+        kind: 'Pod',
+        apiVersion: 'v1',
+        metadata: {
+          name: name
+        },
+        spec: {
+          containers: [{
+            name: 'stencila-container',
+            image: STENCILA_IMAGE,
+            imagePullPolicy: 'IfNotPresent',
+            ports: [{
+              containerPort: port
+            }]
+          }]
+        }
+      }}, (err, pod) => {
+        if (err) return cb(err)
+        const awaitPod = function (cb) {
+          k8s.ns.pods(name).get((err, pod) => {
+            if (err) return cb(err)
+
+            if (pod.status.phase === 'Running') {
+              cb(null, `http://${pod.status.podIP}:${port}`)
+            } else setTimeout(() => awaitPod(cb), 300)
+          })
+        }
+      })
     }
   }
 
-  function onExit (err, data) {
-    if (closed) return
-    if (err) return sink.emit('error', err)
-    self.log.debug('SSE: sending end event', data)
-    sink.write(`event: end\ndata: ${data}\n\n`)
+  pod (session, cb) {
+    if (session.pod) cb(null, session.pod)
+    else {
+      this.spawn((err, pod) => {
+        if (err) return cb(err)
+
+        session.pod = pod
+        cb(null, pod)
+      })
+    }
+  }
+
+  manifest (token, cb) {
+    var session = this.checkin(token)
+    if (!session) session = {}
+
+    // Treat this like a login and remove the session
+    // Another option would be to check id the pod is still active and reuseit
+    if (session.pod) {
+      // TODO: kill the pod if it's still around
+      pino.info('removing pod')
+      session.pod = null
+    }
+    this.pod(session, (err, pod) => {
+      if (err) return cb(err)
+
+      request({
+        method: 'GET',
+        uri: pod,
+        headers: {
+          Accept: 'application/json'
+        }
+      }, {retries: 9}, (err, resp, body) => {
+        const token = this.checkout(session)
+        cb(err, body, token)
+      })
+    })
+  }
+
+  post (type, options, name, token, cb) {
+    const session = this.checkin(token)
+    if (!session) return cb(new Error('Invalid token'))
+    if (!session.pod) return cb(new Error('Session has not been initialised yet'))
+
+    request({
+      method: 'POST',
+      uri: session.pod + '/' + type,
+      headers: {
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(options),
+      timeout: 10000
+    }, {retries: 9}, (err, res, body) => {
+      const token = this.checkout(session)
+      cb(err, body, token)
+    })
+  }
+
+  put (address, method, args, token, cb) {
+    const session = this.checkin(token)
+    if (!session) return cb(new Error('Invalid token'))
+    if (!session.pod) return cb(new Error('Session has not been initialised yet'))
+
+    request({
+      method: 'PUT',
+      uri: session.pod + '/' + address + '!' + method,
+      headers: {
+        Accept: 'application/json'
+      },
+      body: args,
+      json: true
+    }, (err, res, body) => {
+      if (err) return cb(err)
+      cb(err, body, token)
+    })
+  }
+
+  run () {
+    const server = new HostHttpServer(this)
+    server.start()
   }
 }
+
+module.exports = Host
--- HostHttpServer.js
+++ HostHttpServer.js
@@ -1,227 +1,96 @@
-const parseFormdata = require('parse-formdata')
+const body = require('body')
 const cookie = require('cookie')
-const jwt = require('jsonwebtoken')
 const merry = require('merry')
-const uuid = require('uuid')
-const path = require('path')
-const pump = require('pump')
-const send = require('send')
 const url = require('url')
-const fs = require('fs')
 
-const Sibyl = require('./sibyl')
-
-const errors = {
-  EPIPE: function (req, res, ctx, err) {
-    const url = req.url
-    err.message += ' for url ' + url
-    ctx.log.error(err)
-  },
-  EURLNOTFOUND: function (req, res, ctx) {
-    ctx.log.warn('path not found for', req.url)
-    ctx.send(404, { message: 'not found' })
-  },
-  ESESSIONINVALID: function (req, res, ctx, err) {
-    ctx.log.warn('Invalid session ID')
-    ctx.send(403, { message: 'Invalid session ID' })
-  },
-  EFORMPARSE: function (req, res, ctx, err) {
-    var msg = err.message
-    ctx.log.warn(msg)
-    ctx.send(403, { message: msg })
-  },
-  EBETATOKENINVALID: function (req, res, ctx, err) {
-    ctx.log.warn('Invalid token', req.url)
-    res.write(`event: stderr\ndata: Invalid beta token\n\n`)
-  },
-  EINTERNAL: function (req, res, ctx, err) {
-    ctx.log.error(err)
-    ctx.send(500, { message: 'internal server error' })
+class HostHttpServer {
+  constructor (host, address = '127.0.0.1', port = 2000) {
+    this._host = host
+    this._address = address
+    this._port = port
   }
-}
 
-const env = {
-  PORT: 3000,            // Port for the server to listen on
-  TOKEN_SECRET: String,  // JWT token secret should be set as an environment variable
-  DEBUG: '',             // Enable debug logs
-  BETA_TOKEN: 'platypus' // Random client token, required during the beta
-}
+  /**
+   * Start this server
+   */
+  start () {
+    const app = merry()
 
-var opts = { env: env }
-if (process.env.DEBUG) opts.logLevel = 'debug'
-const app = merry(opts)
-const sibyl = Sibyl(app.log)
-
-// launch a container
-app.route('POST', '/~launch', function (req, res, ctx) {
-  parseFormdata(req, function (err, form) {
-    if (err) return errors.EFORMPARSE(req, res, ctx, err)
-
-    // validate input
-    const token = form.fields.token
-    if (token !== ctx.env.BETA_TOKEN) {
-      return errors.EBETATOKENINVALID(req, res, ctx)
+    const error = function (req, res, ctx, code, message) {
+      ctx.log.error('Error', message, req.url)
+      ctx.send(code, { message: message })
     }
 
-    // create opts to start the container
-    let address = form.fields.address
-    const uri = url.parse(req.url, true)
-    const opts = { mock: uri.query && uri.query.mock }
-
-    // image uploaded, stream file to disk & launch image
-    // TODO: make the parts a KV store, and store as files
-    if (form.parts.length) {
-      const source = form.parts[0].stream
-      const location = path.join('/tmp', uuid() + '.tar.gz')
-      const sink = fs.createWriteStream(location)
-
-      pump(source, sink, function (err) {
-        if (err) return errors.EINTERNAL(req, res, ctx, err)
-
-        address = 'file://' + location
-        ctx.log.info('starting container for ' + address)
-        const id = sibyl.open(address, opts)
-        if (id) ctx.send(200, { token: id })
-        else ctx.send(500, { message: 'Error booting image' })
+    const parse = (req, res, ctx, regex, cb) => {
+      const match = url.parse(req.url).pathname.match(regex)
+      if (!match) return error(req, res, ctx, 400, 'Bad Request')
+      const token = cookie.parse(req.headers.cookie || '').token
+      body(req, (err, body) => {
+        if (err) return error(req, res, ctx, 500, err.message)
+        const data = body ? JSON.parse(body) : null
+        cb(match, token, data)
       })
-
-    // no image uploaded, launch container for other protocol
-    } else {
-      // launch container, return id to client
-      ctx.log.info('starting container for ' + address)
-      const id = sibyl.launch(address, opts)
-      if (id) ctx.send(200, { token: id })
-      else ctx.send(500, { message: 'Error booting image' })
     }
-  })
-})
 
-// Connect to an existing SSE stream
-app.route('GET', '~open/:token', function (req, res, ctx) {
-  if (!ctx.params.token) return ctx.send(400, { message: 'No token provided' })
+    const send = (req, res, ctx, body, token) => {
+      let origin = 'http://' + url.parse(req.headers.referer || '').host
+      let headers = {
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Credentials': 'true'
+      }
+      if (token) headers['Set-Cookie'] = `token=${token}`
+      ctx.send(200, body || ' ', headers)
+    }
 
-  var session = sibyl.get(ctx.params.token)
-  if (!session) return ctx.send(400, { message: 'No existing session found for the provided token' })
-
-  // Prevent nginx from buffering the SSE stream
-  if (req.headers['x-nginx']) res.setHeader('X-Accel-Buffering', 'no')
-  res.setHeader('content-type', 'text/event-stream')
-  pump(session, res, function (err) {
-    if (err) errors.EPIPE(req, res, ctx, err)
-  })
-})
-
-// Container session
-app.route([ 'GET', 'POST', 'PUT', 'DELETE' ], '/~session/*', proxyToSession)
-
-// All other non-tilded paths get "rewritten" to
-// container sessions
-app.route([ 'POST', 'PUT', 'DELETE' ], '/*', rewriteToSession)
-app.route('GET', '/*', function (req, res, ctx) {
-  let session = req.headers.referer && req.headers.referer.match(/\/~session\/([^/]+)/)
-  if (!session && (req.url === '/' || req.url.match(/^\/[a-z]+:\/\/.+/))) {
-    const source = send(req, 'dist/index.html')
-    pump(source, res, function (err) {
-      if (err) errors.EPIPE(req, res, ctx, err)
+    app.route('OPTIONS', '/*', (req, res, ctx) => {
+      // CORS headers added to all requests to allow direct access by browsers
+      // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+      res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+      res.setHeader('Access-Control-Max-Age', '1728000')
+      send(req, res, ctx)
     })
-  } else {
-    rewriteToSession(req, res, ctx)
+
+    app.route('GET', '/', (req, res, ctx) => {
+      parse(req, res, ctx, /\//, (match, token, data) => {
+        this._host.manifest(token, (err, manifest, token) => {
+          if (err) return error(req, res, ctx, 500, err.message)
+          send(req, res, ctx, manifest, token)
+        })
+      })
+    })
+
+    app.route('POST', '/*', (req, res, ctx) => {
+      parse(req, res, ctx, /\/(.+)/, (match, token, data) => {
+        const type = match[1]
+        const options = data
+        const name = options && options.name
+        this._host.post(type, options, name, token, (err, address, token) => {
+          if (err) return error(req, res, ctx, 500, err.message)
+          send(req, res, ctx, address, token)
+        })
+      })
+    })
+
+    app.route('PUT', '/*', (req, res, ctx) => {
+      parse(req, res, ctx, /\/([^!]+)!(.+)/, (match, token, data) => {
+        const address = match[1]
+        const method = match[2]
+        const args = data
+        this._host.put(address, method, args, token, (err, result, token) => {
+          if (err) return error(req, res, ctx, 500, err.message)
+          send(req, res, ctx, result, token)
+        })
+      })
+    })
+
+    app.route('default', (req, res, ctx) => {
+      ctx.log.warn('path not found for', req.url)
+      ctx.send(404, { message: 'not found' })
+    })
+
+    app.listen(this._port)
   }
-})
-
-app.route('GET', '/bundle.js', function (req, res, ctx) {
-  const source = send(req, 'dist/bundle.js')
-  pump(source, res, function (err) {
-    if (err) errors.EPIPE(req, res, ctx, err)
-  })
-})
-
-app.route('GET', '/bundle.css', function (req, res, ctx) {
-  const source = send(req, 'dist/bundle.css')
-  pump(source, res, function (err) {
-    if (err) errors.EPIPE(req, res, ctx, err)
-  })
-})
-
-// Serve static files for the Stencila UIs from here rather than from
-// inside session containers
-app.route('GET', '/static/stencila/*', function (req, res, ctx) {
-  const pathname = url.parse(req.url).pathname
-  const source = send(req, path.join('node_modules/stencila/build/', pathname.substring(17)))
-  pump(source, res, function (err) {
-    ctx.log.debug('ending stream', err)
-    if (err) errors.EPIPE(req, res, ctx, err)
-  })
-})
-
-// Handle 404 routes
-app.route('default', errors.EURLNOTFOUND)
-
-// Start the app
-app.listen()
-
-// Proxy/redirect requests to a container session
-function proxyToSession (req, res, ctx) {
-  const match = req.url.match(/\/~session\/([^/]+)((\/)(.*))?/)
-  const token = match[1]
-  const slash = match[3]
-  const path = match[4]
-
-  // Redirect to trailing slash URL so that relative paths in session
-  // requests work as expected
-  if (!slash) {
-    res.statusCode = 301
-    res.setHeader('Location', `/~session/${token}/`)
-    return res.end()
-  }
-
-  jwt.verify(token, ctx.env.TOKEN_SECRET, function (err, payload) {
-    if (err) return errors.ESESSIONINVALID(req, res, ctx, err)
-
-    const url = payload.url
-    if (req.headers['x-nginx']) {
-      // Proxy to session URL using Nginx
-      res.statusCode = 200
-      res.setHeader('X-Accel-Redirect', `/proxy-to-session/${req.method}/${url}/${path}`)
-      // Set a cookie so that subsequent requests to absolute paths can
-      // be rewritten to the session
-      let cookies = cookie.parse(req.headers.cookie || '')
-      if (!cookies.session) res.setHeader('Set-Cookie', cookie.serialize('session', token))
-    } else {
-      // Redirect to session URL
-      res.statusCode = 308
-      res.setHeader('Location', `${url}/${path}`)
-    }
-
-    res.end()
-  })
 }
 
-// Rewrite the URL to point to the session obtained from the
-// `Referer` header or from the `session` cookie
-//
-// Need to use a cookie because `Referer` is not always set or
-// is set but not as a URL including the session token (e.g for fonts)
-// Should we just use the cookie and forget about Referer?
-//
-// This allows us to deal with absolute paths in requests made from
-// HTML & JavaScript hosted within the container. Although this seems
-// a bit hacky, a previous approach required an equal amount of hackyness
-// (and much URL ugliness) within the container hosted HTML/JS and servers.
-function rewriteToSession (req, res, ctx) {
-  let token
-  let referer = req.headers.referer
-  if (referer) {
-    let match = referer.match(/\/~session\/([^/]+)/)
-    if (match) token = match[1]
-  }
-  if (!token) {
-    let cookies = cookie.parse(req.headers.cookie || '')
-    if (cookies.session) token = cookies.session
-  }
-  if (token) {
-    req.url = `/~session/${token}${req.url}`
-    return proxyToSession(req, res, ctx)
-  }
-  errors.EURLNOTFOUND(req, res, ctx)
-}
+module.exports = HostHttpServer
