a073b033ac66b9fe0828a93e59c08cc18b349ccb
Nokome Bentley <me@noko.me>
2018-10-08 18:07:35 +1300

    Add notes on SoftwareSession type

--- src/types.ts
+++ src/types.ts
@@ -13,6 +13,17 @@ type DateTime = string // https://schema.org/DateTime
 
 /**
  * A session (i.e. a container) within a software environment (i.e. an image)
+ * 
+ * There is no schema.org type representing a session or container. So it makes
+ * sense to define something here based the Open Container Initiative (OCI):
+ * 
+ * - https://github.com/opencontainers/runtime-spec/blob/master/config-linux.md
+ * - https://github.com/opencontainers/runtime-spec/blob/master/schema/config-schema.json
+ * - https://github.com/opencontainers/runtime-spec/blob/master/schema/config-linux.json
+ * 
+ * and for the `KubernetesCompiler` to translate it into the Kubernetes API:
+ * 
+ * - https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.12/#container-v1-core
  */
 export class SoftwareSession {
   environment?: SoftwareEnvironment
