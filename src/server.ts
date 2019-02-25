6aeef7a9953e3216709101b8087327efacd036cd
Nokome Bentley <me@noko.me>
2019-02-25 16:27:33 +1300

    feat: add Sentry as option for error reporting

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,13 @@ import { SESSIONS_BASE } from './route-paths'
 
 const app = express()
 
+if (process.env.SENTRY_DSN) {
+  const Sentry = require('@sentry/node')
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN
+  })
+}
+
 app.use(SESSIONS_BASE, bodyParser.raw({ type: '*/*' }))
 app.use('/', router)
 app.listen(2000, () => console.log('Listening on port 2000'))
